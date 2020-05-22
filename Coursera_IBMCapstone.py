8afcb388668ce34c3549b2424ffb550a4812b0e1
tblessett <36075927+tblessett@users.noreply.github.com>
2020-05-21 20:00:43 -0400

    Add files via upload

---
+++ Coursera_IBMCapstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### This python notebook will be used for the Applied Data Science Capstone Project through IBM
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
